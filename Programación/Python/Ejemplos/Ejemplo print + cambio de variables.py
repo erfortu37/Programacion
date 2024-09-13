b17d56820520fa95d07ec1e33c5b68b44dafd2a4
erfortu37 <eric.fortunato@campus.xaloc.org>
2024-09-13 12:34:45 +0200

    Add files via upload

---
+++ Programación/Python/Ejemplos/Ejemplo print + cambio de variables.py
@@ -0,0 +1,15 @@
+var_numero=3
+var_num=3.5
+var_texto="hola"
+
+print (var_numero)
+print (var_numero+var_num)
+var_numero=5
+print (var_numero+var_num)
+
+print (var_texto)
+var_texto="adios"
+print (var_texto)
+
+
+print ("el total es:", var_numero+var_num,"euros, y el cambio es", var_num/var_numero)
