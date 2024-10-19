5b274313b0b97ebc38715a757361283ef3b0dd83
erfortu37 <eric.fortunato@campus.xaloc.org>
2024-10-19 17:01:59 +0200

    Update Ejercicio 11.py

--- Ejercicio 11.py
+++ Ejercicio 11.py
@@ -1,4 +1,4 @@
-# Ejercicio 11
+# Ejercicio 11. Realiza un programa que introduciendo el valor del lado de un cuadrado nos devuelva por pantalla en el área y el perímetro.
 
 Lado=float(input("Dime el lado de un cuadrado:"))
 
