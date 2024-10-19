b26e9400d20a11c9a1ef597b2c19b2eb76bd377b
erfortu37 <eric.fortunato@campus.xaloc.org>
2024-10-19 17:03:11 +0200

    Update Ejercicio 3.py

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -1,4 +1,4 @@
-# Ejercicio 3 (uso del input)
+# Ejercicio 3. Programa que pida dos números enteros y realice la suma correspondiente
 
 var1=int(input ("introduce un número:"))
 var2=int(input ("introduce otro número:"))
